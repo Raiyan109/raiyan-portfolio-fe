9d66030e3936738b5c013c7f242c0e843084bf84
Raiyan109 <kraiyan109@gmail.com>
2023-11-16 15:26:22 +0600

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,51 @@
+import './contact.css'
 
 const Contact = () => {
     return (
         <div>
+            <div className="card">
+                <h2 className=''>Contact Me</h2>
+                <div className="row">
+                    <div className="col">
+                        <div className="form-group">
+                            <label>First Name</label>
+                            <input type="text" />
+                        </div>
+                    </div>
 
+                    <div className="col">
+                        <div className="form-group">
+                            <label>Surname</label>
+                            <input type="text" />
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input type="text" />
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="form-group">
+                            <label>Phone</label>
+                            <input type="text" />
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="form-group">
+                            <label>Message</label>
+                            <textarea></textarea>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <input type="submit" value="Submit" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
