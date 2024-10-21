1da1400944b49555f2bef9d554b9905d719799f1
Raiyan109 <kraiyan109@gmail.com>
2024-10-21 15:14:12 +0600

    responsive sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -12,37 +12,19 @@ import Contact from './components/contact/Contact'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import ScrollVelozity from './components/scrollVelozity/ScrollVelozity'
-import About2 from './components/about/About2'
-import { useState } from 'react'
 
 AOS.init()
 
 function App() {
-  const [isActive, setActive] = useState(false);
 
-  const toggleClass = () => {
-    setActive(!isActive);
-  };
 
   return (
     <>
-      <div>
-        {/* <svg className={`${isActive ? 'hamRotate ' : null} ham ham1`}
-          onClick={toggleClass} viewBox="0 0 100 100" width="80" >
-          <path
-            className="line top"
-            d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40" />
-          <path
-            className="line middle"
-            d="m 30,50 h 40" />
-          <path
-            className="line bottom"
-            d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" />
-        </svg> */}
-        <div className='hidden md:block'>
+      <div className=''>
+        <div className=''>
           <Sidebar />
         </div>
-        <div className="main">
+        <div className="ml-0 md:ml-[80px]">
           <Home />
           <About />
           <ScrollVelozity />
@@ -53,7 +35,6 @@ function App() {
           {/* <Testimonials /> */}
           <Blog />
           <Contact />
-          <About2 />
         </div>
       </div>
     </>
